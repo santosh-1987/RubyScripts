40e853d578d3df12b308f400dce563b52230ce1a
Santosh Kumar Mohanty <santa.jyp@gmail.com>
2017-03-20 14:10:18 +0530

    Added Solutions for 10_001 Prime & Product of 13 digit num

---
+++ ProjectEuler/Problem7.rb
@@ -0,0 +1,28 @@
+=begin
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?
+=end
+require "rubygems"
+class PrimeNum
+  def self.fetch_list(limit)
+    arr = []
+    start = 2
+     loop do
+       arr << start if is_prime?(start)
+       start += 1
+       break if arr.size ==limit
+     end
+     return arr
+  end
+
+  def self.is_prime?(number)
+    for i in 2..(number-1)
+      return false if number%i == 0
+    end
+    return true
+  end
+end
+
+print PrimeNum.fetch_list(10_001).last
+# print PrimeNum.is_prime?(1)
