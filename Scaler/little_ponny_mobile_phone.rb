5dd9a66dca6fc012baf3d0ff50e044ed4d96c45c
Santosh Mohanty <Santosh.Mohanty@cerner.com>
2022-01-29 19:17:25 +0530

    Add Little Ponny with Binary Search
    Signed-off-by: Santosh Mohanty <Santosh.Mohanty@cerner.com>

--- Scaler/little_ponny_mobile_phone.rb
+++ Scaler/little_ponny_mobile_phone.rb
@@ -6,19 +6,45 @@ class Solution
   # @return an array of integers
   def solve(a, b)
     solution = []
-    b.each do |amount|
-      count = 0
-      a.each do |model_price|
-        if amount - model_price >= 0
-          amount = amount - model_price
-          count += 1
-        else
-          break
-        end
-      end
-      solution << count
+    prefix_sum_arr = []
+    prev = 0
+    a.each do |ele|
+      prefix_sum_arr << prev + ele
+      prev += ele
     end
+
+    b.each do |amount|
+      location = search_max(prefix_sum_arr, amount)
+      if location.nil?
+        solution << 0
+      else
+        solution << a[0..location].count
+      end
+    end
+
     return solution
+  end
+
+  def search_max(array, element)
+    start_index = 0
+    end_index = array.length - 1
+    location = nil
+
+    while start_index <= end_index
+      mid = (start_index + end_index) / 2
+      if (array[mid] == element)
+        location = mid
+        break
+      end
+
+      if (element > array[mid])
+        location = mid
+        start_index = mid +1
+      else
+        end_index = mid -1
+      end
+    end
+    return location
   end
 end
 
