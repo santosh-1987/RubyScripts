ddfdbd314ddd60f0bb4cc2e47e552ccb3562e7e1
Santosh Mohanty <Santosh.Mohanty@cerner.com>
2022-02-12 14:56:07 +0530

    Added Final Code
    Signed-off-by: Santosh Mohanty <Santosh.Mohanty@cerner.com>

--- Scaler/code_test/sisters_love_to_sing.rb
+++ Scaler/code_test/sisters_love_to_sing.rb
@@ -1,36 +1,43 @@
 require 'pry'
-
+# This needs to be fixed
 def main(singing_minutes)
   # singing_minutes = gets.strip.split(" ").map(&:to_i)
-  ananya_start, ananya_end = [singing_minutes[0], singing_minutes[1]]
-  bhavya_start, bhavya_end = [singing_minutes[2], singing_minutes[3]]
-  result = 0
-  if (ananya_start == bhavya_start)
-    if (ananya_end > bhavya_end)
-      result = bhavya_start - bhavya_end
-    else
-      result = ananya_start - ananya_end
-    end
-  end
+  as, ae = [singing_minutes[0], singing_minutes[1]]
+  bs, be = [singing_minutes[2], singing_minutes[3]]
 
-  if ((bhavya_end - bhavya_start) > (ananya_end - ananya_start))
-    result = ananya_end - ananya_start
+  if as == bs && ae == be
+    puts ae - as
+  elsif as == bs && ae > be
+    puts be - bs
+  elsif as == bs && be > ae
+    puts ae - as
+  elsif as > bs && be >= ae
+    puts ae - as
+  elsif bs > as && be <= ae
+    puts be - bs
+  elsif bs > as && be > ae
+    puts ae - bs
+  elsif as < bs && ae < be
+    puts be - as
   else
-    result = bhavya_end - bhavya_start
+    puts 0
   end
-
-  if (ananya_start < bhavya_start)
-    result = ananya_end - bhavya_start
-  else
-    result = ananya_start - bhavya_end
-  end
-
-  puts result
 end
 
-singing_minutes1 = [3, 4, 1, 11]
-singing_minutes2 = [2, 5, 4, 6]
-singing_minutes3 = [5, 6, 0, 5]
+singing_minutes1 = [3, 4, 1, 11] # 1
+singing_minutes2 = [2, 5, 4, 6] # 1
+singing_minutes3 = [5, 6, 0, 5] # 0
+singing_minutes4 = [7, 12, 8, 13] # 4
+singing_minutes5 = [3, 8, 6, 14] # 2
+singing_minutes6 = [0, 6, 0, 6] # 6
+singing_minutes7 = [0, 0, 0, 1] # 0
+singing_minutes8 = [0, 0, 0, 0] # 0
+
 main(singing_minutes1)
 main(singing_minutes2)
 main(singing_minutes3)
+main(singing_minutes4)
+main(singing_minutes5)
+main(singing_minutes6)
+main(singing_minutes7)
+main(singing_minutes8)
