08f9de0a2677062553b2d0da84af833764031f89
malayswain <40353759+malayswain@users.noreply.github.com>
2022-03-13 22:04:51 +0530

    Update alternating_subarray.rb
    changed a name.

--- Scaler/sub_array/alternating_subarray.rb
+++ Scaler/sub_array/alternating_subarray.rb
@@ -5,10 +5,10 @@ You have to tell all the indices of array A that can act as a center of 2 * B + 
 
 A 0-1 alternating array is an array containing only 0's & 1's, and having no adjacent 0's or 1's. For e.g. arrays
 [0, 1, 0, 1], [1, 0] and [1] are 0-1 alternating, while [1, 1] and [0, 1, 0, 0, 1] are not.
-Example Input
+Example i/p
 Input 1:
 
- A = [1, 0, 1, 0, 1,23]
+ A = [1, 0, 1, 0, 1]
  B = 1
 Input 2:
 
