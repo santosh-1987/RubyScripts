842ec3e5689b584543dad57a41da6e05c84261f6
sm062703 <Santosh.Mohanty@cerner.com>
2022-03-18 14:23:26 +0530

    Added Maximum Sub Array problem

--- Scaler/sub_array/maximum_subarray.rb
+++ Scaler/sub_array/maximum_subarray.rb
@@ -5,6 +5,7 @@ Example
 Input: [-2,1,-3,4,-1,2,1,-5,4],
 Output: 6
 Explanation: [4,-1,2,1] has the largest sum = 6
+#https://chenyunzhang.medium.com/find-the-maximum-subarray-with-ruby-approach-1174aee76c2c
 =end
 require 'pry'
 def max_sub_array(nums)
