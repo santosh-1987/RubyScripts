8f87abe017381f4f91dcc7289d3b629e8d655213
sm062703 <Santosh.Mohanty@cerner.com>
2022-03-18 14:22:53 +0530

    Added Maximum Sub Array problem

--- Scaler/sub_array.rb
+++
@@ -1,8 +0,0 @@
-class Solution
-  # @param a : array of integers
-  # @return an integer
-  def solve(a)
-    coun
-    a.combination(2).to_a
-  end
-end
---
+++ Scaler/sub_array/maximum_subarray.rb
@@ -0,0 +1,18 @@
+=begin
+Question Overview
+You are given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+Example
+Input: [-2,1,-3,4,-1,2,1,-5,4],
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6
+=end
+require 'pry'
+def max_sub_array(nums)
+  largest_sum, current = nums[0], 0
+  nums.each do |x|
+    largest_sum = [largest_sum, current = [x, current + x].max].max
+  end
+  largest_sum
+end
+
+print max_sub_array([-2,1,-3,4,-1,2,1,-5,4])
