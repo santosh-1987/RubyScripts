826eda06fd5c381f4680a0c3027da751fc9ed358
sm062703 <Santosh.Mohanty@cerner.com>
2022-07-07 21:21:19 +0530

    Added Merge Sort

--- Scaler/sorting/array_sorting.rb
+++ Scaler/sorting/array_sorting.rb
@@ -4,6 +4,7 @@ Selection Sort Algorithm selects the minimum element from current index and swap
 TC: O(N^2)
 Unstable Sorting Algorithm
 =end
+require 'pry'
 
 class Array
   def self.selection_sort(arr)
@@ -37,9 +38,33 @@ class Array
     end
     return arr
   end
+
+  def self.merge_sort arr
+    return arr if arr.length == 1
+    midpoint = (arr.length - 1)/2
+    puts "arr: #{arr}"
+    arr_1 = merge_sort(arr[0..midpoint])
+    arr_2 = merge_sort(arr[(midpoint+1)..(arr.length-1)])
+    puts "---- Calling: arr1: #{arr_1}, arr2: #{arr_2}"
+    return merge(arr_1, arr_2)
+  end
+
+  def self.merge arr_1, arr_2
+    arr = []
+    while arr_1.length > 0 && arr_2.length > 0
+      if arr_1[0] < arr_2[0]
+        arr.push(arr_1.shift)
+      else
+        arr.push(arr_2.shift)
+      end
+    end
+    arr.push(arr_1).flatten! if arr_1.length > 0
+    arr.push(arr_2).flatten! if arr_2.length > 0
+    return arr
+  end
 end
 
-print Array.bubble_sort([3, 7, 1, 16, 5, -2, 8])
+# print Array.bubble_sort([3, 7, 1, 16, 5, -2, 8])
 # print Array.bubble_sort([2, 1, 5, 3, 4])
-puts
 # print Array.bubble_sort([-1, 1, 8, 2, 3])
+# print Array.merge_sort([3, 7, 1, 16, 5, -2, 8])
