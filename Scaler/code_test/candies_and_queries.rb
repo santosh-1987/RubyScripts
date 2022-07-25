bc18536173d512d8914ab96b641d0f6267ee42b3
sm062703 <Santosh.Mohanty@cerner.com>
2022-07-25 19:16:44 +0530

    Added Code Test Problems

---
+++ Scaler/code_test/candies_and_queries.rb
@@ -0,0 +1,25 @@
+require 'pry'
+class Solution
+  # @param a : integer
+  # @param b : array of array of integers
+  # @param c : array of integers
+  # @return an array of integers
+  def solve(a, b, c)
+    arr = Array.new(a) { 0 }
+    ans = []
+    b.each do |pair|
+      left = pair[0] - 1
+      right = pair[1] - 1
+      left.upto(right).each do |index|
+        arr[index] = arr[index] + 1
+      end
+    end
+
+    c.each do |query|
+      ans << arr.select { |elem| elem >= query }.count
+    end
+    return ans
+  end
+end
+
+print Solution.new.solve(4, [[1, 2], [4, 4], [1, 3]], [1, 2, 3])
---
+++ Scaler/code_test/no_more_zeros.rb
@@ -0,0 +1,15 @@
+class Solution
+  # @param a : integer
+  # @return a string
+  def solve(a)
+    return a.next unless a%10 == 9
+
+    count = a.next.to_s.count('0')
+    num = a + ("1"*count).to_i
+    return num.next
+  end
+end
+
+# puts Solution.new.solve(9)
+# puts Solution.new.solve(11)
+puts Solution.new.solve(39)
